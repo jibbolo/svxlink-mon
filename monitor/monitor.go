21562ccfc40ee0d6c913b1cd171c88a202783864
Giacomo Marinangeli <jibbolo@gmail.com>
2017-12-20 20:07:24 +0100

    New monitor test cmd

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/jibbolo/svxlink-mon/broker"
+)
+
 // Thu Sep 28 15:32:09 2017: IW0HKS: Login OK from 195.94.189.122:63358
 // Fri Oct  6 20:01:16 2017: IR0UFQ: Client 44.208.124.17:38551 disconnected: Connection closed by remote peer
 
@@ -13,9 +20,13 @@ type RadioLink struct {
 }
 
 func main() {
-	// broker, err := broker.NewAWSBroker("eu-west-1", ", "", "", "mytopic")
-	// if err != nil {
-	// 	log.Fatalf("Can't init msg broker: %v", err)
-	// 	return
-	// }
+	broker, err := broker.NewAWSBroker("", "", "", "", "mytopic")
+	if err != nil {
+		log.Fatalf("Can't init msg broker: %v", err)
+		return
+	}
+	broker.Subscribe(func(msg []byte) {
+		fmt.Printf("--> %s\n", msg)
+	})
+	select {}
 }
